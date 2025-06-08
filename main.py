593184cb96e4d244a180b8b723375266c67c0dcc
Caitlyn-Xinyi <chencxy@umich.edu>
2025-06-08 15:35:57 -0400

    change

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
 
+# your existing routes...
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 # --- Config ---
 ERP_API_BASE = "https://Vintech-CZ.on.plex.com/api/datasources/"
 
